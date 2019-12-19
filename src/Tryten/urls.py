5119fb462bd769f383f8f49ee179ea1f3c3e6080
KIngSimeone <53562471+KIngSimeone@users.noreply.github.com>
2019-12-19 16:49:10 +0100

    Bootstrap added

--- src/Tryten/urls.py
+++ src/Tryten/urls.py
@@ -21,7 +21,8 @@ from profiles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about/',views.about,name='about')
 ]
  
 
